48ef31951ddb0084d8207155a6888f3756dacb53
Mitchell Magro <mitchell.magro@fabillio.com>
2022-05-09 13:06:02 +0200

    AX-3662: fixes some more styling

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div className='h-screen w-screen w-full wrapper flex justify-center items-center'>
         <div className='w-3/5 min-w-600 border shadow-lg'>
-        <div className='text-black font-bold p-16 bg-white bg-opacity-50'>    
+        <div className='text-black font-bold md:p-12 lg:p-16 p-4 bg-white bg-opacity-50'>    
             <div className='container mx-auto 2xl'>
                 <div className='header w-2/6 m-auto mt-4'>  
                 <Image url='./assets/img/main-logo.png'/>
@@ -37,7 +37,7 @@ class App extends Component {
                         </a>
                     </li>
                     <li>
-                        <a href="#" class="mr-3 hover:underline md:mr-3 !text-grey">Velkommen til</a>
+                        <a href="#" class="mr-3 md:mr-3 !text-grey">Velkommen til</a>
                     </li>
                     <li className='flex justify-center items-center'>
                         <a href="#" class="mr-4 hover:underline md:mr-6 !text-grey">
