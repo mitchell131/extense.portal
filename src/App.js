6b5a39d4a8c726e1c4cdbe5a213f54401cd42f20
Mitchell Magro <mitchell.magro@fabillio.com>
2022-05-17 11:18:52 +0200

    AX-3662: Added proper variables

--- src/App.js
+++ src/App.js
@@ -36,33 +36,33 @@ const App = () => {
   },[auth.isAuthenticated , auth.isLoading])
 
 
-  // if (!auth.isAuthenticated && !auth.isLoading && ref.current === false) {
-  //   auth.signinRedirect().catch((resp) => addToast('Server Offline', { appearance: 'error', autoDismiss: true }))
-  //   ref.current = true
-  // }
+  if (!auth.isAuthenticated && !auth.isLoading && ref.current === false) {
+    auth.signinRedirect().catch((resp) => addToast('Server Offline', { appearance: 'error', autoDismiss: true }))
+    ref.current = true
+  }
 
-  // if (isAuthenticated) {
-  //   window.history.pushState({}, '', '/')
-  //   var singleSystem = auth.user?.profile.single_system
-  //   switch (singleSystem) {
-  //     case "AO":
-  //       window.location.replace(process.env.AO_REDIRECT_URL)
-  //       return
-  //     case "AI":
-  //       window.location.replace(process.env.AI_REDIRECT_URL)
-  //       return
-  //     case "OO":
-  //       window.location.replace(process.env.OO_REDIRECT_URL)
-  //       return
-  //   }
-  // }
+  if (isAuthenticated) {
+    window.history.pushState({}, '', '/')
+    var singleSystem = auth.user?.profile.single_system
+    switch (singleSystem) {
+      case "AO":
+        window.location.replace(process.env.AO_REDIRECT_URL)
+        return
+      case "AI":
+        window.location.replace(process.env.AI_REDIRECT_URL)
+        return
+      case "OO":
+        window.location.replace(process.env.OO_REDIRECT_URL)
+        return
+    }
+  }
 
   return (
     <>
     <Navbar onClickhandler={()=> auth.signoutRedirect()} isLoggedIn={isAuthenticated}/>
     <div className='h-screen w-screen w-full wrapper flex justify-center items-center'>
       {auth.isLoading && <LoadingSpinner />}
-      {true && (
+      {isAuthenticated && (
         <div className='w-3/5 min-w-650 max-w-5xl border shadow-lg'>
           <div className='text-black lg:p-16 p-4 bg-white bg-opacity-60'>
             <div className='container mx-auto 2xl'>
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({onClickhandler, isLoggedIn}) => {
     <nav className="flex items-center justify-between flex-wrap bg-black p-4 w-full fixed">
     <div className="w-full block flex-grow flex  justify-end">
       <div className='logout'>
-           <a href="#" onClick={onClickhandler} className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white">Logg ut</a>
+      {isLoggedIn && <a href="#" onClick={onClickhandler} className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white">Logg ut</a>}
       </div>
     </div>
   </nav>
