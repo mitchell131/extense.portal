d464d477022802e6d6d806621a7287595eb268d0
Mitchell Magro <mitchell.magro@fabillio.com>
2022-05-18 17:56:09 +0200

    AX-3662: Added dropdown for logout button

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
+import Dropdown from '../dropdown/dropdown.jsx';
 
 const Navbar = ({ onClickhandler, isLoggedIn, userEmail }) => {
   return (
     <nav className="flex items-center justify-between flex-wrap bg-black p-4 w-full fixed">
       <div className="w-full block flex-grow flex  justify-end">
         <div className='logout'>
-          {isLoggedIn && <a href="#" onClick={onClickhandler} className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white">{userEmail}</a>}
+        {isLoggedIn &&  <Dropdown text={userEmail} onClickhandler={onClickhandler}/>}
         </div>
-        <>
-        </>
       </div>
     </nav>
   );
